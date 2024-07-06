abf024ecd3d78e2eb33eac3e7798735d83a2fae5
Zotta <frikilinux@gmail.com>
2024-07-05 23:15:19 -0300

    Modificando algunas cositas

--- src/main/java/com/wildproyect/literalura/Repository/BookRepository.java
+++ src/main/java/com/wildproyect/literalura/Repository/BookRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-
 public interface BookRepository extends JpaRepository<Book, Long> {
     Book findBookByTitulo(String nombre);
-    List<Book> findBookByLanguajeContaining(String languaje);
+
+    List<Book> findBookByLanguageContaining(String language);
 }
